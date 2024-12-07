05162eb074a78001e7afcb7bf0eace49c28ec7ab
ivangarsin <ivangarshin333@gmail.com>
2024-12-07 16:10:25 +0300

    all

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
+#include "task1.h"
+#include "task2_1.h"
+#include "task2_2.h"
+#include "task3.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    alg_task1();
+    alg_task2_1();
+    alg_task2_2();
+    alg_task3();
     return 0;
 }
