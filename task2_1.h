5cf3766e3e6ec88b21ec802bb0b4df852ead1cbb
ivangarsin <ivangarshin333@gmail.com>
2024-12-07 16:24:55 +0300

    all

--- task2_1.h
+++ task2_1.h
@@ -7,33 +7,7 @@
 #include <limits>
 #include <fstream>
 
-// Константы
-const double g = 9.81;                 // Ускорение свободного падения, м/с²
-const double m_initial = 98000;        // Масса ЛА, кг
-const double S = 201;                  // Площадь крыла, м²
-const double rho0 = 1.225;             // Плотность воздуха на уровне моря, кг/м³
-const double H_start = 300;            // Начальная высота, м
-const double H_end = 5000;             // Конечная высота, м
-const double V_start = 83.33;          // Начальная скорость, м/с
-const double V_end = 222.22;           // Конечная скорость, м/с
-const double H_step = 100;             // Шаг высоты, м
-const double V_step = 10;              // Шаг скорости, м/с
-const double Cp = 0.86;                // Удельный расход топлива, кг/Н·ч
-const double P_total = 103000 * 3;     // Суммарная тяга двигателей, Н
-
-// Функция для расчета плотности воздуха
-double air_density(double h) {
-    if (h < 11000) {
-        return rho0 * pow(1 - 0.0065 * h / 288.15, 4.256);
-    } else {
-        return rho0 * 0.22336 * exp(-0.0001577 * (h - 11000));
-    }
-}
-
-// Аппроксимация зависимости Cx от Cy
-double calculate_Cx(double Cy) {
-    return 0.02 + 0.04 * Cy * Cy;
-}
+#include "const_and_algs.h"
 
 int alg_task2_1() {
     // Сетка высот и скоростей
--- task3.h
+++ task3.h
@@ -6,25 +6,7 @@
 #include <cmath>
 #include <limits>
 
-const double g = 9.81;                 // Ускорение свободного падения, м/с²
-const double m = 98000;                // Масса ЛА, кг
-const double S = 201;                  // Площадь крыла, м²
-const double rho0 = 1.225;             // Плотность воздуха на уровне моря, кг/м³
-const double H_start = 300;            // Начальная высота, м
-const double H_end = 5000;             // Конечная высота, м
-const double V_start = 83.33;          // Начальная скорость, м/с
-const double V_end = 222.22;           // Конечная скорость, м/с
-const double H_step = 100;             // Шаг высоты, м
-const double V_step = 10;              // Шаг скорости, м/с
-
-// Функция для расчета плотности воздуха
-double air_density(double h) {
-    if (h < 11000) {
-        return rho0 * pow(1 - 0.0065 * h / 288.15, 4.256);
-    } else {
-        return rho0 * 0.22336 * exp(-0.0001577 * (h - 11000));
-    }
-}
+#include "const_and_algs.h"
 
 // Функция для расчета времени перехода между высотами
 double calculate_time(double H1, double H2, double V, double Cy) {
